73e01a049aa01ae87652c3d91850d566714dc885
test patel <test.patel008@gmail.com>
2025-11-13 20:34:01 +0530

    Update base URL

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://www.amazon.in/',
+    baseUrl: 'https://www.saucedemo.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       return config;
